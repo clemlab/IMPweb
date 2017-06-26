5e48ddffa29e56f7092f7d25637a8746be9c70e4
cynest <charlesnelson1.5@gmail.com>
2017-06-26 12:32:16 -0700

    added secondary navbar and switched template folder

--- webform/funfile.py
+++ webform/funfile.py
@@ -1,4 +1,5 @@
 # Current implementation is all jokes
+from time import sleep
 
 def length(stuff):
 	return len(stuff)
@@ -7,4 +8,5 @@ def rev(stuff):
 	return stuff[::-1]
 
 def spam(stuff):
+	sleep(60)
 	return 'text of paradise lost goes here'
